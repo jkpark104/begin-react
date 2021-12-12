edd7ae899618cb33c5b2211d7becc09f4d3c6745
jkpark104 <jkrang104@gmail.com>
2021-12-12 13:24:36 +0900

    [학습] React
    1. useState

--- src/InputSample.js
+++ src/InputSample.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function InputSample() {
+  const [inputs, setInputs] = useState({
+    name: '',
+    nickname: '',
+  });
+
+  const { name, nickname } = inputs;
+
+  const onChange = e => {
+    const { name, value } = e.target;
+
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
+  const onReset = () => {
+    setInputs({
+      name: '',
+      nickname: '',
+    });
+  };
+
   return (
     <div>
-      <input />
-      <button>초기화</button>
+      <input name='name' placeholder='이름' onChange={onChange} value={name} />
+      <input name='nickname' placeholder='닉네임' onChange={onChange} value={nickname} />
+      <button onClick={onReset}>초기화</button>
       <div>
         <b>값: </b>
+        {name} {nickname}
       </div>
     </div>
   );
